93c812966bea9669abb588bb767d1c075ee1ba1b
Eugene Komarov <gene.komarov@gmail.com>
2023-11-16 15:00:09 +0500

    Починили линтер

--- projects/poker-calc-v2/client/app/src/App.tsx
+++ projects/poker-calc-v2/client/app/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from '@/Common/Test';
+import Page from '@/Application/Page';
 
 function App() {
   return (
     <div className="App">
-      <Test/>
+      <Page/>
     </div>
   );
 }
