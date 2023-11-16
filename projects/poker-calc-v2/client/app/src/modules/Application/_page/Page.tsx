67072461dde12bdf1d9e77d04a331526cc5851bb
Eugene Komarov <gene.komarov@gmail.com>
2023-11-16 18:59:17 +0500

    Сверстали сетку

--- projects/poker-calc-v2/client/app/src/modules/Application/_page/Page.tsx
+++ projects/poker-calc-v2/client/app/src/modules/Application/_page/Page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import theme from '@/Common/themes/dark/theme.module.scss';
 import styles from '@/Application/_page/Page.module.scss';
+import classNames from 'classnames';
+import Content from "@/Application/_page/Page/Content";
 
 export default function Page(): React.JSX.Element {
     return (
-        <div className={theme.dark}>
-            sadasd
-            <div className={styles.inner}>qweqwe</div>
+        <div className={classNames(theme.dark, styles.panel)}>
+            <Content/>
         </div>
     );
 }
