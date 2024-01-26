20e8e677f2d96b85002c174b98d14b572fe5dbde
ep.komarov <ep.komarov@tensor.ru>
2024-01-26 16:08:36 +0500

    Deal

--- projects/poker-calc-v2/client/poker-odds-engine/src/Card.ts
+++ projects/poker-calc-v2/client/poker-odds-engine/src/Card.ts
@@ -32,40 +32,71 @@ export class Card {
     }
 }
 
-interface IDealMap {
+interface ICardsMap {
     [cardName: string]: Card;
 }
 
-/** */
+/** Раздача */
 export class Deal {
 
-    private _map: IDealMap = {};
-    private _orders: number[];
+    private _cardsMap: ICardsMap = {};
+    private _cardNames: string[] = [];
+    private _droppedNames: string[] = [];
 
-    constructor() {
-
+    constructor(cards: Card[]) {
+        cards.forEach((card) => {
+            const cardName = card.toString();
+            if (this._cardsMap.hasOwnProperty(cardName)) {
+                throw(new Error(`Карта ${cardName} уже есть в выборке`));
+            } else {
+                this._cardsMap[cardName] = card;
+                this._cardNames.push(cardName);
+            }
+        });
     }
 
-    includes(): boolean {
-
+    includes(cardName: string): boolean {
+        return !!(this._cardNames.includes(cardName) && this._cardsMap[cardName]);
     }
 
     shuffle(): void {
 
     }
 
-    getByCount(count: number): Deal {
-
+    pullByCount(count: number): Deal {
+        const cardNames = this._cardNames.splice(0, count);
+        this._droppedNames.push(...cardNames);
+        const cards = this._getCardsByNames(cardNames);
+        return new Deal(cards);
     }
 
-    getByCards(cards: Card[]): Deal {
-
-    }
-
-    getCards(): Card[] {
-        return this._orders.map((order) => {
-            return
+    pullByCards(cardNames: string[]): Deal {
+        const cards = cardNames.map((cardName) => {
+            if (this.includes(cardName)) {
+                const card = this._cardsMap[cardName];
+                this._droppedNames.push(this._cardNames.splice());
+            } else {
+                throw(new Error(`Карты нет в выборке: ${cardName}`));
+            }
         });
+    }
+
+    getAllCards(): Card[] {
+        return this._getCardsByNames(this._cardNames);
+    }
+
+    private _getCardsByNames(cardNames: string[]): Card[] {
+        return cardNames.map((cardName) => {
+            const card = this._cardsMap[cardName];
+            if (!card) {
+                throw(new Error(`Нет информации о карте ${cardName}`));
+            }
+            return this._cardsMap[cardName];
+        });
+    }
+
+    get length(): number {
+        return this._cardNames.length;
     }
 }
 
@@ -79,6 +110,9 @@ function genDeck(): Card[] {
     });
     return cards;
 }
+
+
+export const deal = new Deal(genDeck());
 
 // /** Масти */
 // const SUITS = ['D', 'H', 'C', 'S'] as const;
