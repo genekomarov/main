32f88cf79f4ee7bfc0d698f601e1c05316d718be
Eugene Komarov <gene.komarov@gmail.com>
2024-01-29 21:09:11 +0500

    Deal

--- projects/poker-calc-v2/client/poker-odds-engine/src/Card.ts
+++ projects/poker-calc-v2/client/poker-odds-engine/src/Card.ts
@@ -39,64 +39,46 @@ interface ICardsMap {
 /** Раздача */
 export class Deal {
 
-    private _cardsMap: ICardsMap = {};
-    private _cardNames: string[] = [];
-    private _droppedNames: string[] = [];
+    private _cards: ICardsMap = {};
 
     constructor(cards: Card[]) {
         cards.forEach((card) => {
-            const cardName = card.toString();
-            if (this._cardsMap.hasOwnProperty(cardName)) {
-                throw(new Error(`Карта ${cardName} уже есть в выборке`));
-            } else {
-                this._cardsMap[cardName] = card;
-                this._cardNames.push(cardName);
-            }
+            this._add(card);
         });
     }
 
+    /** Добавить карту */
+    private _add(card: Card): void {
+        const cardName = card.toString();
+        if (this.includes(cardName)) {
+            throw(new Error(`Карта уже содержится в наборе: ${cardName}`));
+        } else {
+            this._cards[cardName] = card;
+        }
+    }
+
     includes(cardName: string): boolean {
-        return !!(this._cardNames.includes(cardName) && this._cardsMap[cardName]);
+        return this._cards.hasOwnProperty(cardName);
     }
 
     shuffle(): void {
 
     }
 
-    pullByCount(count: number): Deal {
-        const cardNames = this._cardNames.splice(0, count);
-        this._droppedNames.push(...cardNames);
-        const cards = this._getCardsByNames(cardNames);
-        return new Deal(cards);
+    pullCount(count: number): Deal {
+
     }
 
     pullByCards(cardNames: string[]): Deal {
-        const cards = cardNames.map((cardName) => {
-            if (this.includes(cardName)) {
-                const card = this._cardsMap[cardName];
-                this._droppedNames.push(this._cardNames.splice());
-            } else {
-                throw(new Error(`Карты нет в выборке: ${cardName}`));
-            }
-        });
+
     }
 
     getAllCards(): Card[] {
-        return this._getCardsByNames(this._cardNames);
-    }
-
-    private _getCardsByNames(cardNames: string[]): Card[] {
-        return cardNames.map((cardName) => {
-            const card = this._cardsMap[cardName];
-            if (!card) {
-                throw(new Error(`Нет информации о карте ${cardName}`));
-            }
-            return this._cardsMap[cardName];
-        });
+        return Object.values(this._cards);
     }
 
     get length(): number {
-        return this._cardNames.length;
+        return Object.keys(this._cards).length;
     }
 }
 
