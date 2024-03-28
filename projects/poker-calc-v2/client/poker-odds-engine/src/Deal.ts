0007e0ff107a9dc6069d362b9117a8b957ae40f3
ep.komarov <ep.komarov@tensor.ru>
2024-03-28 18:44:56 +0500

    Добавили документацию

--- projects/poker-calc-v2/client/poker-odds-engine/src/Deal.ts
+++ projects/poker-calc-v2/client/poker-odds-engine/src/Deal.ts
@@ -1,5 +1,11 @@
+// Классы
 export {default as Deal} from 'src/_deal/Deal';
+
+// Хелперы
+export {genDeck, sortRunksUp} from 'src/_deal/helpers';
+
+// Константные типы
 export {TSuit, TRunk, TCardName, RUNKS, RUNK} from 'src/_deal/consts';
 
-export {genDeck, sortRunksUp} from 'src/_deal/helpers';
+// Чистые типы
 export {ICard, IDeal} from 'src/_deal/interface';
