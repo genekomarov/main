167c47d5bec894b0158185a4dabcd6ed2e71b041
Eugene Komarov <gene.komarov@gmail.com>
2024-04-06 16:58:44 +0500

    doc

--- projects/poker-calc-v2/client/poker-odds-engine/src/common.ts
+++ projects/poker-calc-v2/client/poker-odds-engine/src/common.ts
@@ -1,3 +1,5 @@
+// Хелперы
 export {stringFromArray, genString, genSpaceString} from 'src/_common/helpers/string';
 
+// Чистые типы
 export {INode, INodeWithSubNodes} from 'src/_common/interface/nodes';
