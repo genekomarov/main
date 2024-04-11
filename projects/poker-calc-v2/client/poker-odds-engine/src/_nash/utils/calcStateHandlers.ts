b12f2b9f8aaea93c8d1dd550c84bf2c48d12cc35
Eugene Komarov <gene.komarov@gmail.com>
2024-04-11 19:58:30 +0500

    calcElementWinProbability

--- projects/poker-calc-v2/client/poker-odds-engine/src/_nash/utils/calcStateHandlers.ts
+++ projects/poker-calc-v2/client/poker-odds-engine/src/_nash/utils/calcStateHandlers.ts
@@ -3,16 +3,18 @@ import {TCalcStateHandlers, TNodes} from 'src/_nash/interface/ICalcStateHandlers
 /** Обработчики расчета состояния */
 export const CALC_STATE_HANDLERS: TCalcStateHandlers = {
     root: [],
-    elements: [calcElementProbability],
+    elements: [calcElementWinProbability],
     combs: []
 };
 
-function calcElementProbability(nodes: TNodes): void {
+function calcElementWinProbability(nodes: TNodes): void {
     const node = nodes[1];
     if (!node) return;
     const {count, wins} = node.data;
     node.data.winProbability = count ? wins / count * 100 : 0;
 }
+
+
 
 // function printElementName(nodes: TNodes): void {
 //     console.log(nodes[1]?.data.key);
