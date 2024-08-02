fea6ae57ce929d997a9414adac938af77ae932c1
ep.komarov <ep.komarov@tensor.ru>
2024-08-02 10:09:44 +0500

    remove mathOperations

--- projects/poker-calc-v2/client/poker-odds-engine/src/common/helpers/mathOperations.ts
+++
@@ -1,4 +0,0 @@
-/** Делит числа или возвращает переданное значение */
-export function calcPercentOrValue(dividend: number, divider: number, value: number): number {
-    return divider ? dividend / divider * 100 : value;
-}
