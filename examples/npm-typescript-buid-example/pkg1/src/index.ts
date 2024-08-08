8c07e40babef5fe3e1507ad777e3041d210fc889
ep.komarov <ep.komarov@tensor.ru>
2024-08-08 10:51:20 +0500

    npm ts build example

---
+++ examples/npm-typescript-buid-example/pkg1/src/index.ts
@@ -0,0 +1,12 @@
+import func from 'src/code/code';
+import newfunc from 'src/new/code';
+
+console.log(func(1, 2));
+console.log(newfunc('a', 'b'));
+
+export {func, newfunc};
+// export newfunc;
+
+// export const a = 11;
+// export {default as func} from 'src/code/code';
+// export {default as newfunc} from 'src/new/code';
---
+++ examples/npm-typescript-buid-example/pkg2/src/index.ts
@@ -0,0 +1,4 @@
+import {func, newfunc} from 'pkg1';
+
+console.log(func(1, 2));
+console.log(newfunc(String(1), String(2)));
